d56461473377184f4c1e9701088d11cf8535df2e
luncheon <takeuck+github@gmail.com>
2018-06-04 13:56:05 +0900

    fix: `event.preventDefault()` does not set `event.defaultPrevented` on IE

--- src/script/event.ts
+++ src/script/event.ts
@@ -1,6 +1,13 @@
 export function dispatchCustomEvent(target: EventTarget, type: string, bubbles: boolean, cancelable: boolean, detail?: any) {
   const event = document.createEvent('CustomEvent')
   event.initCustomEvent(type, bubbles, cancelable, detail)
+
+  // fix: `event.preventDefault()` does not set `event.defaultPrevented` on IE
+  // https://stackoverflow.com/questions/23349191/event-preventdefault-is-not-working-in-ie-11-for-custom-events
+  event.preventDefault = function () {
+    Object.defineProperty(this, 'defaultPrevented', { get() { return true } })
+  }
+
   target.dispatchEvent(event)
   return event
 }
